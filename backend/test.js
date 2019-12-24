ecb1ec93a015f8c0db5ca523f79ed5dc46361709
357095 <zhunshunli001@deppon.com>
2019-12-24 09:17:46 +0800

    rre

--- backend/test.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-setInterval(() => {
-    request('http://localhost:3000', function (error, response, body) {
-        console.log(body)//打印百度首页html内容
-    })
-}, 1000)
-
--- frontend/build.sh
+++
@@ -1,2 +0,0 @@
-docker build -t taro .
-docker run -v $PWD/dist:/usr/src/app/dist taro
